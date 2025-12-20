80b564ace188a215c90e26d1d0402a44b2aafb29
Yanzhen-CN <2609313038@qq.com>
2025-12-20 19:52:01 +0800

    agent interaction improved

--- model/model_interface.py
+++ model/model_interface.py
@@ -74,7 +74,7 @@ def init_model(
         return
 
     kwargs = dict(
-        torch_dtype=dtype,
+        dtype=dtype,
         low_cpu_mem_usage=True,
         trust_remote_code=True,
         use_flash_attn=use_flash_attn,
